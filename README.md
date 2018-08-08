59e684f5ef1e36869da293350a593910ebb21ef6
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-08-08 13:01:51 -0400

    First Push

---
+++ README.md
@@ -0,0 +1,1 @@
+Attempt at Making A Character Generator
---
+++ src/main/java/io/jdsalisbury/github/chargen/CharGenApplication.java
@@ -0,0 +1,12 @@
+package io.jdsalisbury.github.chargen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharGenApplication.class, args);
+	}
+}
