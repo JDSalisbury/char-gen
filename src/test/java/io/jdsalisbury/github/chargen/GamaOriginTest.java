74a5fda2a74acfa4c1cc61114c45a36d9aadb62b
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-08-08 20:32:22 -0400

    adding some getters

--- src/test/java/io/jdsalisbury/github/chargen/GamaOriginTest.java
+++ src/test/java/io/jdsalisbury/github/chargen/GamaOriginTest.java
@@ -1,5 +1,18 @@
+
 package io.jdsalisbury.github.chargen;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
 
 public class GamaOriginTest {
 
+	@Test
+	public void shouldReturnOriginName() {
+		GamaOrigin vampire = new GamaOrigin("Vampire", "STR", "Conspiracy", "Dark", 0, 0, 0, 0);
+
+		String name = vampire.getOriginName();
+		assertThat(name, is("Vampire"));
+	}
 }
