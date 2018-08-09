8d86801e51a861285f1b4795b6e644e05fe92c02
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-08-08 20:41:35 -0400

    getting some test down

--- src/main/java/io/jdsalisbury/github/chargen/GamaOrigin.java
+++ src/main/java/io/jdsalisbury/github/chargen/GamaOrigin.java
@@ -1,8 +1,32 @@
 package io.jdsalisbury.github.chargen;
 
 public class GamaOrigin {
+	private String origin;
+	private String ability;
+	private String skill;
+	private String bonus;
+	private int ac;
+	private int fort;
+	private int ref;
+	private int will;
 
 	public GamaOrigin(String origin, String ability, String skill, String bonus, int ac, int fort, int ref, int will) {
+		this.origin = origin;
+		this.ability = ability;
+		this.skill = skill;
+		this.bonus = bonus;
+		this.ac = ac;
+		this.fort = fort;
+		this.ref = ref;
+		this.will = will;
+	}
+
+	public String getName() {
+		return origin;
+	}
+
+	public String getAbility() {
+		return ability;
 	}
 
 }
--- src/test/java/io/jdsalisbury/github/chargen/GamaOriginTest.java
+++ src/test/java/io/jdsalisbury/github/chargen/GamaOriginTest.java
@@ -8,11 +8,17 @@ import org.junit.Test;
 
 public class GamaOriginTest {
 
+	private GamaOrigin vampire = new GamaOrigin("Vampire", "STR", "Conspiracy", "Dark", 0, 0, 0, 0);
+
 	@Test
 	public void shouldReturnOriginName() {
-		GamaOrigin vampire = new GamaOrigin("Vampire", "STR", "Conspiracy", "Dark", 0, 0, 0, 0);
-
-		String name = vampire.getOriginName();
+		String name = vampire.getName();
 		assertThat(name, is("Vampire"));
+	}
+
+	@Test
+	public void shouldReturnAbility() {
+		String ability = vampire.getAbility();
+		assertThat(ability, is("STR"));
 	}
 }
