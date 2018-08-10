eb88bc6ea20197475618e542740ba06af3d75891
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-08-10 10:10:26 -0400

    setting up the Origin processor, should grab the data and combine it and ship me out info for a character sheet

--- src/main/java/io/jdsalisbury/github/chargen/OriginProcessor.java
+++ src/main/java/io/jdsalisbury/github/chargen/OriginProcessor.java
@@ -1,10 +1,16 @@
 package io.jdsalisbury.github.chargen;
 
-import java.util.List;
-import java.util.Random;
-
 public class OriginProcessor {
+	private String type;
 
+	public void setOriginType(GamaOrigin primary, GamaOrigin secondary) {
+		String primaryType = primary.getOrigin();
+		String secondaryType = secondary.getOrigin();
+		this.type = primaryType + " " + secondaryType;
+	}
 
-	
+	public String getOriginType() {
+		return type;
+	}
+
 }
--- src/test/java/io/jdsalisbury/github/chargen/OriginProcessorTest.java
+++ src/test/java/io/jdsalisbury/github/chargen/OriginProcessorTest.java
@@ -1,12 +1,28 @@
 package io.jdsalisbury.github.chargen;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class OriginProcessorTest {
 
-	@Test
-	public void shouldCombineTwoOriginsIntoACharacter() {
+	private GamaOrigin vampire = new GamaOrigin("Vampire", "STR", "Conspiracy", "Dark", 0, 0, 0, 0);
+	private GamaOrigin yeti = new GamaOrigin("Yeti", "STR", "Nature", "Bio", 1, 0, 0, 0);
 
+	@Test
+	public void shouldSetName() {
+		String primary = vampire.getOrigin();
+		String secondary = yeti.getOrigin();
+
+		OriginProcessor processOrigins = new OriginProcessor();
+		processOrigins.setOriginType(vampire, yeti);
+		String type = processOrigins.getOriginType();
+
+		assertThat(type, is("Vampire Yeti"));
 	}
 
+	// 18 ability score of your Primary Origin.
+	// 16 ability score of your Secondary Origin.
+	// both same ability, then you get a 20 ability score
 }
